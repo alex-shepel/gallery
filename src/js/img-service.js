beccd2b454c482c1b135852046ff3571eaf9932e
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-11-15 19:19:16 +0200

    move constant module fields to class

--- src/js/img-service.js
+++ src/js/img-service.js
@@ -1,41 +1,41 @@
 import { Notify } from 'notiflix';
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com';
-const RESOURCE = 'api';
-
-const PARAMS = {
-  key: '24268385-a09efe65560efa0dec086fa93',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  editors_choice: 'true',
-  safesearch: 'true',
-  per_page: '40',
-};
-
-const FILTERS = [
-  'webformatURL',
-  'largeImageURL',
-  'tags',
-  'likes',
-  'views',
-  'comments',
-  'downloads',
-];
-
-const NO_MATCHES_MESSAGE =
-  'Sorry, there are no images matching your search query. Please try again.';
-const COLLECTION_END_MESSAGE =
-  "We're sorry, but you've reached the end of search results.";
-const GET_INFO_BEFORE_FETCH_ERROR = `You must make query before trying to get its info.`;
-
 export default class ImgService {
+  static #BASE_URL = 'https://pixabay.com';
+  static #RESOURCE = 'api';
+
+  static #PARAMS = {
+    key: '24268385-a09efe65560efa0dec086fa93',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    editors_choice: 'true',
+    safesearch: 'true',
+    per_page: '40',
+  };
+
+  static #FILTERS = [
+    'webformatURL',
+    'largeImageURL',
+    'tags',
+    'likes',
+    'views',
+    'comments',
+    'downloads',
+  ];
+
+  static #NO_MATCHES_MESSAGE =
+    'Sorry, there are no images matching your search query. Please try again.';
+  static #COLLECTION_END_MESSAGE =
+    "We're sorry, but you've reached the end of search results.";
+  static #GET_INFO_BEFORE_FETCH_ERROR = `You must make query before trying to get its info.`;
+
   #page = 1;
   #totalHits = null;
   #query = null;
 
   #axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: ImgService.#BASE_URL,
     headers: {
       Accept: 'application/json',
     },
@@ -47,10 +47,12 @@ export default class ImgService {
     const params = {
       q: query,
       page: pageNum,
-      ...PARAMS,
+      ...ImgService.#PARAMS,
     };
 
-    const { data } = await this.#axiosInstance(RESOURCE, { params });
+    const { data } = await this.#axiosInstance(ImgService.#RESOURCE, {
+      params,
+    });
     const filteredData = ImgService.#filterResponseData(data);
 
     ImgService.#checkNoMatches(data.totalHits);
@@ -67,15 +69,16 @@ export default class ImgService {
   }
 
   #checkCollectionEnd(currentPage) {
-    const isCollectionEnd = currentPage * PARAMS.per_page >= this.#totalHits;
+    const isCollectionEnd =
+      currentPage * ImgService.#PARAMS.per_page >= this.#totalHits;
     if (isCollectionEnd) {
-      throw new Error(COLLECTION_END_MESSAGE);
+      throw new Error(ImgService.#COLLECTION_END_MESSAGE);
     }
   }
 
   static #checkNoMatches(totalHits) {
     if (totalHits === 0) {
-      throw new Error(NO_MATCHES_MESSAGE);
+      throw new Error(ImgService.#NO_MATCHES_MESSAGE);
     }
   }
 
@@ -88,7 +91,7 @@ export default class ImgService {
   }
 
   static #filterResponseHit(hit) {
-    return FILTERS.reduce((filteredObj, filter) => {
+    return ImgService.#FILTERS.reduce((filteredObj, filter) => {
       filteredObj[filter] = hit[filter];
       return filteredObj;
     }, {});
@@ -97,6 +100,6 @@ export default class ImgService {
   get totalHits() {
     if (this.#totalHits) return this.#totalHits;
 
-    throw new Error(GET_INFO_BEFORE_FETCH_ERROR);
+    throw new Error(ImgService.#GET_INFO_BEFORE_FETCH_ERROR);
   }
 }
